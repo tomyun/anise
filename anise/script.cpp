9596de25077d63238cbe5b0a40b21e70f2d8e02a
Kyungdahm Yun <tomyun@gmail.com>
2004-02-21 23:53:22 +0000

    Add script support for isle

--- anise/script.cpp
+++ anise/script.cpp
@@ -260,6 +260,10 @@ void Script::setOpcodes()
 		//HACK: same as aisimai
 		opcode_oput_list = opcode_aisimai_oput_list;
 	}
+	else if (option->game_type == GAME_ISLE) {
+		//HACK: same as aisimai
+		opcode_oput_list = opcode_aisimai_oput_list;
+	}
 }
 
 
