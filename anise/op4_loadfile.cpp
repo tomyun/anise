53aa28a90d8027800dc5f46fcd1aa74b9446291d
Kyungdahm Yun <tomyun@gmail.com>
2004-02-24 11:14:02 +0000

    Add hack for Kakyusei

--- anise/op4_loadfile.cpp
+++ anise/op4_loadfile.cpp
@@ -3,19 +3,26 @@
 SCRIPTCALL Script::op4_loadFile()
 {
 	char filename[PARAMETER_LENGTH];
+	word offset;
 
 	parameter = getParameter();
-	parameter->load(0, filename);
+	//HACK: for kakyusei
+	if (parameter->getType(0) == PARAMETER_TYPE_CONSTANT) {
+		parameter->load(1, filename);
+		offset = parameter->get(2);
+	}
+	else {
+		parameter->load(0, filename);
+		offset = parameter->get(1);
+	}
+	deleteParameter();
 
-	word offset = parameter->get(1);
 	MemorySegment *segment = memory->getCurrentSegment();
 	MemoryBlock *memory_block = segment->get(&offset);
 
 	file->open(filename);
 	file->load(memory_block, offset);
 	file->close();
-
-	deleteParameter();
 
 	return RETURN_NORMAL;
 }
